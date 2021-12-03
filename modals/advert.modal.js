bc0de41264bef6d68d77825fe035b5f1888933cf
kadirbastimar <58986912+kadirbastimar@users.noreply.github.com>
2021-12-04 02:00:30 +0300

    .

--- modals/advert.modal.js
+++ modals/advert.modal.js
@@ -7,7 +7,7 @@ const AdvertSchema = new mongoose.Schema(
         description: { type: String, required: true, minlength: 20, maxlength: 1000 },
         specs: [{
             spec: { type: String, required: true },
-            decs: { type: String, required: true },
+            desc: { type: String, required: true },
             mandatory: { type: Boolean, required: true, default: false }
         }],
         is_cargo_accepts: { type: Boolean, required: true, default: true },
